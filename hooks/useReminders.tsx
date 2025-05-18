b8dc05a6b6cac5b13856c2b93c1653906c790eef
Edvan Ribeiro Dos Santos Junior <jk.jr47@gmail.com>
2025-05-18 02:18:23 +0000

    implementar o hook useReminders com funcionalidade aprimorada para recuperar e gerenciar lembretes

---
+++ hooks/useReminders.tsx
@@ -0,0 +1,36 @@
+import { useAppContext } from "@/context/AppContext";
+import { Reminder } from "@/types/Reminder";
+
+interface UseRemindersReturn {
+  getAllReminders: () => Reminder[];
+  getLastActiveReminders: () => Reminder[];
+  getReminderById: (reminderId: string) => Reminder | undefined;
+  addReminder: (reminder: Reminder) => void;
+}
+
+export const useReminders = (): UseRemindersReturn => {
+  const { reminders = [], addReminder } = useAppContext()
+
+  const getAllReminders = (): Reminder[] => reminders;
+
+const getLastActiveReminders = (): Reminder[] => {
+  return reminders
+    .filter((reminder) => reminder.active)
+    .sort((a, b) => {
+      const aDate = new Date(a.createdAt);
+      const bDate = new Date(b.createdAt);
+      return bDate.getTime() - aDate.getTime();
+    })
+    .slice(-5)
+    .reverse();
+};
+  const getReminderById = (reminderId: string): Reminder | undefined =>
+    reminders.find((r) => r.id === reminderId);
+
+  return {
+    getAllReminders,
+    getReminderById,
+    getLastActiveReminders,
+    addReminder,
+  };
+};
